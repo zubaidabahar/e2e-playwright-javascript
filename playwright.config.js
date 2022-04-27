a8992c706097ec1fb65cba314588d4ee796bab3c
zubaidabahar <zubaidabahar00@gmail.com>
2022-04-27 11:19:28 +0200

    set headless to true

--- playwright.config.js
+++ playwright.config.js
@@ -33,6 +33,7 @@ const config = {
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: true,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -40,8 +41,6 @@ const config = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-
-    headless: false
   },
 
   /* Configure projects for major browsers */
